5a7f3ef749ca89e789dd5ec9c2580ba1f1656e8c
r.williams <richard.williams@logius.nl>
2025-01-05 17:58:34 +0100

    set port to 3000

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+  },
   esbuild: {
     loader: "jsx",
     include: /src\/.*\.jsx?$/

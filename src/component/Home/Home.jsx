9b7ad2759b8954ebab1853d86d0e3503029e50aa
Erin <juries.erin@gmail.com>
2025-01-05 17:55:07 +0100

    Fixed Home section ayout

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -17,20 +17,20 @@ export const Home = () => {
     }, [jwt]);
 
     return (
-        <div className='pb-10'>
-            <section className='banner -z-50 relative flex flex-col justify-center items-center'>
+        <section className='pb-10'>
+            <div className='banner relative flex flex-col justify-center items-center'>
                 <div className='w-[50vw] z-10 text-center'>
                     <p className='text-2xl lg:text-6xl font-bold py-5'>My Food App</p>
                     <p className='text-gray-300 text-xl lg:text-4xl'>Taste the convenience!</p>
                 </div>
                 <div className='cover absolute top-0 left-0 right-0' />
                 <div className='fade-out' />
-            </section>
-            <section className='p-10 lg:py-10 lg:px-20'>
+            </div>
+            <div className='p-10 lg:py-10 lg:px-20'>
                 <p className='text-2xl font-semibold text-gray-400 py-3 pb-10'>Top Meals</p>
                 <MultiItemCarousel />
-            </section>
-            <section className='px-5 lg:px-20 pt-10'>
+            </div>
+            <div className='px-5 lg:px-20 pt-10'>
                 <h1 className='text-2xl font-semibold text-gray-400 py-8'>
                     Order From Our Handpicked Favourites
                 </h1>
@@ -39,7 +39,7 @@ export const Home = () => {
                         <RestaurantCard key={item.id} item={item} />
                     ))}
                 </div>
-            </section>
-        </div>
+            </div>
+        </section>
     );
 };
